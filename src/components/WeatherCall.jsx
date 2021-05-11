18ba27a29a9f94b83196dca59ab31e58e0206d6e
platiniradukunda <platinirad@gmail.com>
2021-05-11 09:11:22 -0400

    working app

--- src/components/WeatherCall.jsx
+++ src/components/WeatherCall.jsx
@@ -6,7 +6,7 @@ class WeatherCall extends Component {
         return (
             <div>
                 {/* Rendering the data to the DOM */}
-                <h3>{this.props.clouds}</h3> 
+                <h3 className="clouds">{this.props.clouds}</h3> 
                 <h1 className="temp">{this.props.data} &deg;</h1>    
             </div> 
         );
